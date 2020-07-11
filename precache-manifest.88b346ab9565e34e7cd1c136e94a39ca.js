d90c3d9cd363013afe86ac23da47aabd9d167712
Abhinandan-Pandey <abhinandanpandey.ap@gmail.com>
2020-07-12 01:33:07 +0530

    Updates

---
+++ precache-manifest.88b346ab9565e34e7cd1c136e94a39ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c010222e43f2925efb4430a207f2f76",
+    "url": "/StoryClub/index.html"
+  },
+  {
+    "revision": "398060bf62a31eb79375",
+    "url": "/StoryClub/static/css/main.697c4f61.chunk.css"
+  },
+  {
+    "revision": "1718b48915ed47b3e96b",
+    "url": "/StoryClub/static/js/2.4bc3f18e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StoryClub/static/js/2.4bc3f18e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398060bf62a31eb79375",
+    "url": "/StoryClub/static/js/main.fdc2b413.chunk.js"
+  },
+  {
+    "revision": "43fc1c4e1cebcc200409",
+    "url": "/StoryClub/static/js/runtime-main.9e23d568.js"
+  },
+  {
+    "revision": "45ee5357949f4b54ae4fffe20ca69575",
+    "url": "/StoryClub/static/media/cover_page.45ee5357.jpg"
+  },
+  {
+    "revision": "417b38a54f366b6654c780b39ae25655",
+    "url": "/StoryClub/static/media/sprite.417b38a5.svg"
+  }
+]);
